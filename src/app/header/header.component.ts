52cb0ac52d96ecc670ed44f677ff031f91a85422
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-09-12 13:45:01 +0300

    Improve form adaptive layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
 
   openDialog() {
     this.dialog.open(DialogComponent, {
-      width: 'min-content'
+      minWidth: 'fit-content',
     });
   }
 
