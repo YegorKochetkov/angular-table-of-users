21a938cd064bb4f173c8e60c192c1bd9f78bf6b1
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-09-14 08:27:57 +0300

    Add user details component

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ApiService } from 'src/app/services/api.service';
+import { UserInterface } from 'src/app/types/user.type';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+  user: UserInterface | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.api.getUser(id)
+      .subscribe({
+        next: (user) => {
+          this.user = user;
+        },
+        error: (error) => {
+          console.error('error while fetching users: ', error.message)
+        }
+      });
+  }
+}
