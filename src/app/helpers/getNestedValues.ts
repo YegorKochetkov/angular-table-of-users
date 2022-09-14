5647b6587fa82dbe43c08be3d4a30c10dd65e45e
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-09-14 22:58:47 +0300

    Update helper

--- src/app/helpers/getNestedValues.ts
+++ src/app/helpers/getNestedValues.ts
@@ -1,9 +1,9 @@
 export const getNestedValues = (data: object, values: string[]) => {
-  if(!(data instanceof Array) && typeof data === 'object') {
+  if (!(data instanceof Array) && typeof data === 'object') {
     Object.values(data).forEach((value: string | Object) => {
-      if(!(value instanceof Array) && typeof value === 'object'){
+      if (!(value instanceof Array) && typeof value === 'object'){
          getNestedValues(value, values);
-      } else if (!(value instanceof Array)) {
+      } else if (typeof value === 'string') {
         values.push(value);
       }
    });
