989e10d13387297c9135c7c790fb1e032c5df706
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-09-14 08:28:06 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TableComponent } from 'src/app/components/table/table.component';
+import { DetailsComponent } from 'src/app/components/details/details.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: TableComponent },
+  { path: 'details/:id', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
