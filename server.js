544d185f8bd4d44213931a01ae0d6cfb428a9646
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-09-22 12:28:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './dist'
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
